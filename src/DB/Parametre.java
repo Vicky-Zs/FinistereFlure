d4264c2ef3c1a18e7a4681aa6a17dafe49357211
MrKillingJoke <47780552+MrKillingJoke@users.noreply.github.com>
2019-04-22 17:23:00 +0200

    BDD2

--- src/DB/Parametre.java
+++ src/DB/Parametre.java
@@ -2,7 +2,7 @@ package DB;
 
 public interface Parametre {
     /*  Permet d'avoir variables fixes*/
-        static final String chemin ="jdbc:mysql://FinstereFlure.alwaysdata.net:3306";
+        static final String chemin ="jdbc:mysql://localhost/finstereflure";
         static final String user = "Observateur";
         static final String password = "projetJava2019";
 }
