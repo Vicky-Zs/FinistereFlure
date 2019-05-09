4bee4b22ff9456d17d9246661b3c49486cb96516
Vicky Zweistein <a.zweistein@gmx.com>
2019-05-09 16:46:28 +0200

    JavDoc DataBase

--- src/DB/DataBase.java
+++ src/DB/DataBase.java
@@ -18,12 +18,24 @@ public class DataBase implements Parametre {
   private String demande;
   private String comptePseudo;
   private String compteMdp;
+  /**
+   * Permet de récupérer le Pseudo
+   * @return [pseudo]
+   */
   private String getPseudo(){
     return this.comptePseudo;
   }
+  /**
+   * Récupère le mot de passe
+   * @return [motdepasse]
+   */
   private String getPassword(){
     return this.compteMdp;
   }
+  /**
+   * Permet de se connecter
+   * @return [Connection]
+   */
   public static Connection openBDD(){
     Connection con = null;
     ResultSet res;
@@ -39,25 +51,36 @@ public class DataBase implements Parametre {
     }
     return con;
   }
-  public static Connection closeConnexion(Connection co) {
+
+  /**
+   * Coupe la connexion
+   * @param  co [L'ancienne connexion]
+   */
+  public static void closeConnexion(Connection co) {
     // permet de se connecter à la DB
     if (co != null) {
       try {
         co.close();
         System.out.println("Database connection terminated.");
       } catch (Exception e) {}}
-    return co;
   }
+  /**
+   * Permet de supprimer une ligne du tableau des scores
+   * @param p  [Joueur]
+   * @param co [Connexion]
+   */
   public void supprimerLigneScore(Player p,Connection co){
-    // permet de supprimer une ligne dans le tableau de score
     try {
       Statement sup = co.createStatement();
       String pseudo = "'"+p.getPseudo()+"'";
       sup.executeUpdate("DELETE FROM game"+" WHERE Pseudo = "+pseudo);
     }catch (Exception e){}}
-
+    /**
+     * Permet d'insérer dans le tableau des scores une nouvelle ligne
+     * @param p  [Joueur]
+     * @param co [Connexion]
+     */
   public void insererLigne(Player p, Connection co){
-    // permet d'inserer dans la table de score une nouvelle ligne
     try {
       int dead = 4 - p.getNbToken();
       Statement insert = co.createStatement();
@@ -72,6 +95,10 @@ public class DataBase implements Parametre {
       }
       return res;
     }
+    /**
+     * Permet de créer un compte
+     * @param co [Connexion]
+     */
     public void creerCompte(Connection co){
       Scanner sc = new Scanner(System.in);
       System.out.println("Inserez un pseudo ");
@@ -110,10 +137,15 @@ public class DataBase implements Parametre {
           exist = true;
         }
       }
+      sc.close();
       return exist;
     }
+    /**
+     * Permet de changer un pseudo
+     * @param  co           [Connexion]
+     * @throws SQLException
+     */
     public void changerPseudo(Connection co)throws SQLException{
-      /*Partie ou l'on gere le changement de pseudo*/
       Scanner sc = new Scanner(System.in);
       System.out.println("Votre nouveau pseudo c'est ? ");
       String pseudo = new String();
@@ -125,7 +157,13 @@ public class DataBase implements Parametre {
       Statement changePseudo = co.createStatement();
       changePseudo.executeUpdate("UPDATE compte SET Pseudo ="+pseudo+"WHERE Pseudo ="+compte);
       changePseudo.executeUpdate("UPDATE game SET Pseudo ="+pseudo+"WHERE Pseudo ="+compte);
+      sc.close();
     }
+    /**
+     * Permet de changer un mot de passe
+     * @param  co           [Connexion]
+     * @throws SQLException
+     */
     public void changerMdp(Connection co) throws SQLException{
       Scanner sc = new Scanner(System.in);
       System.out.println("Votre nouveau mot de passe c'est ? ");
@@ -134,8 +172,13 @@ public class DataBase implements Parametre {
       mdp="'"+mdp+"'";
       Statement changeMDP = co.createStatement();
       changeMDP.executeUpdate("UPDATE compte SET Password ="+mdp+"WHERE Pseudo ="+this.comptePseudo);
+      sc.close();
     }
-
+    /**
+     * Permet de changer une adresse mail
+     * @param  co           [Connexion]
+     * @throws SQLException
+     */
     public void changerMail(Connection co)throws SQLException{
       Scanner sc = new Scanner(System.in);
       System.out.println("Votre nouvelle adrese mail c'est ? ");
@@ -144,8 +187,14 @@ public class DataBase implements Parametre {
       mail="'"+mail+"'";
       Statement changeEmail = co.createStatement();
       changeEmail.executeUpdate("UPDATE compte SET Email ="+mail+"WHERE Pseudo ="+this.comptePseudo);
+      sc.close();
     }
-
+    /**
+     * Permet de se connexter en tant qu'utilisateur
+     * @param  co           [Connexion]
+     * @return              [boolean pour savoir si l'identifiant et le mot de passe correspond]
+     * @throws SQLException
+     */
     public boolean coUser(Connection co)throws SQLException{
       Scanner sc = new Scanner(System.in);
       System.out.println("Entrez votre pseudo : ");
@@ -181,10 +230,16 @@ public class DataBase implements Parametre {
       else{
         System.out.println("\nAucun compte existant\n");
       }
+      sc.close();
       return connect;
     }
 
     //SELECT Password FROM `compte` WHERE Pseudo = 'nico'
+    /**
+     * Permet de se déconnecter en utilisateur
+     * @param  connected [Boolean pour savoir si une personne est connectée]
+     * @return           [Boolean pour savoir si une personne est connectée]
+     */
     public boolean decoUser(boolean connected){
         return !connected;
     }
