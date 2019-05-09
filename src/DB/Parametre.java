8a813a57026e6b638f27f1a28a0c1c4b10f2f1cf
Vicky Zweistein <a.zweistein@gmx.com>
2019-05-09 12:17:19 +0200

    MàJ Paramètre

--- src/DB/Parametre.java
+++ src/DB/Parametre.java
@@ -1,8 +1,8 @@
 package DB;
 
 public interface Parametre {
-    /*  Permet d'avoir variables fixes*/
-        static final String chemin ="jdbc:mysql://localhost/finstereflure";
-        static final String user = "Observateur";
-        static final String password = "projetJava2019";
+  /*  Permet d'avoir variables fixes*/
+  static final String chemin ="jdbc:mysql://localhost/finstereflure";
+  static final String user = "Observateur";
+  static final String password = "projetJava2019";
 }
