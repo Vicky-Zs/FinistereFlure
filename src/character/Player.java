db01ab726abdebec837d0f8308f4e69002c087fc
Vicky Zweistein <a.zweistein@gmx.com>
2019-05-09 12:00:10 +0200

    MàJ Player

--- src/character/Player.java
+++ src/character/Player.java
@@ -1,20 +1,16 @@
 package character;
-
 import finstereflure.*;
+import map.*;
+import token.*;
+import map.*;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.regex.Pattern;
-import map.*;
-import token.*;
-
 import java.util.Scanner;
-import map.*;
-
 /**
  *
  * @author Nicolas
  */
-
 public class Player {
   private HashSet<TokenP> token = new HashSet<>(); // encapsulation en TokenP apres
   protected String pseudo;
@@ -23,107 +19,80 @@ public class Player {
   private boolean connected = false;
   private int partieJouee = 0;
 
-
-                /*Constructeur de la class player*/
-
-  /*public Player(HashSet<Token> token, String nomPerso){
-      for(Token p : token){
-          this.token.add(p);
-      }
-      this.pseudo = nomPerso;
-  }*/
-
   public Player(String pseudo, int playerId, Game g){
     this.pseudo = pseudo;
     this.token.add(new TokenP(g, 1, playerId));
     this.token.add(new TokenP(g, 3, playerId));
     this.token.add(new TokenP(g, 4, playerId));
     this.token.add(new TokenP(g, 5, playerId));
+    if(!(this instanceof IA)){
+      this.mail = mail();
+      this.password = password();
+    }
   }
-
-                /*  Méthodes  */
-  private String pseudo(){
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Veuillez votre pseudo :");
-      String pseudo = sc.nextLine();
-      System.out.println("Voici ton p'tit nom  : " + pseudo);
-      return pseudo;
+  private String mail(){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Veuillez votre adressse mail :");
+    String pseudo = sc.nextLine();
+    System.out.println("Voici votre adresse mail  : " + adresseMail);
+    sc.close();
+    return pseudo;
   }
-  
-    private String mail(){
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Veuillez votre adressse mail :");
-      String pseudo = sc.nextLine();
-      System.out.println("Voici votre adresse mail  : " + adresseMail);
-      return pseudo;
+  private String password(){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Veuillez votre mot de passe :");
+    String pseudo = sc.nextLine();
+    sc.close();
+    return pseudo;
   }
-    
-    private String password(){
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Veuillez votre mot de passe :");
-      String pseudo = sc.nextLine();
-      return pseudo;
+  public int getNbToken(){
+    // permet de recuperer le nombre de tokens encore en vie
+    int cpt = 0;
+    for(TokenP t : token){
+      cpt++;
+    }
+    return cpt;
   }
-   
-  public int getNbToken(){ // Modif: Private -> Protected
-      // permet de recuperer le nombre de tokens encore en vie
-      int cpt = 0;
-      for(TokenP t : token){
-          cpt++;
-      }
-      return cpt;
-  }
-
   public String getMail(){
-      return adresseMail;
+    return adresseMail;
   }
-  
-    public int getGame(){
-      return partieJouee=0;
+  public int getGame(){
+    return partieJouee=0;
   }
-  
-  
   public String getPseudo(){
-      return pseudo;
+    return pseudo;
   }
-  
   public String getPassword(){
-      return password;
+    return password;
   }
-  
   public boolean getConnected(){
-      return connected;
+    return connected;
   }
-
   public void setConnected(boolean isCo){
-      this.connected = isCo;
+    this.connected = isCo;
   }
   @Override
   public String toString(){
-      return this.pseudo +" tu as encore "+getNbToken()+" tokens.";
+    return this.pseudo +" tu as encore "+getNbToken()+" tokens.";
   }
-
   public boolean isEmpty() {
-      return token.isEmpty();
+    return token.isEmpty();
   }
-
   public HashSet getToken() {
-      return token;
+    return token;
   }
-
   public TokenP getToken(int pattern) {
-      
-      for (TokenP t : token) {
-          if (t instanceof TokenP) {
-              TokenP tP = (TokenP) t;
-              if (tP.getPatternA() == pattern) {
-                  return t;
-              }
-              else if (tP.getPatternB() == pattern) {
-                  return t;
-              }
-          }
+    for (TokenP t : token) {
+      if (t instanceof TokenP) {
+        TokenP tP = (TokenP) t;
+        if (tP.getPatternA() == pattern) {
+          return t;
+        }
+        else if (tP.getPatternB() == pattern) {
+          return t;
+        }
       }
-      return null;
+    }
+    return null;
   }
 }
